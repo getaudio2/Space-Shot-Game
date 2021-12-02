e627405fe9edba003eb2f2b99bb9a723b5c00cfb
Paul <paugermaisar@hotmail.es>
2021-12-02 15:41:33 +0100

    Editing shipmovement script

--- Space Shot Game/Assets/ShipMovement.cs
+++ Space Shot Game/Assets/ShipMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+    float moveSpeed = 3;
+
+    bool moveUp;
+    bool moveDown;
+    bool moveLeft;
+    bool move
+
+
     // Start is called before the first frame update
     void Start()
     {
