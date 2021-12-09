5e6bc3dc9ceb3f31e3fb336f1e6ee0c963144665
Paul <paugermaisar@hotmail.es>
2021-12-09 15:20:33 +0100

    Added ship movement

--- Space Shot Game/Assets/ShipMovement.cs
+++ Space Shot Game/Assets/ShipMovement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    float moveSpeed = 3;
+    float moveSpeed = 10;
 
     bool moveUp;
     bool moveDown;
     bool moveLeft;
-    bool move
+    bool moveRight;
 
 
     // Start is called before the first frame update
@@ -21,6 +21,41 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveUp = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        moveDown = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+        moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        float moveAmount = moveSpeed * Time.fixedDeltaTime;
+        Vector2 move = Vector2.zero;
+
+        if (moveUp)
+        {
+            move.y += moveAmount;
+        }
+
+        if (moveDown)
+        {
+            move.y -= moveAmount;
+        }
+
+        if (moveLeft)
+        {
+            move.x -= moveAmount;
+        }
+
+        if (moveRight)
+        {
+            move.x += moveAmount;
+        }
+
+        pos += move;
+
+        transform.position = pos;
     }
 }
